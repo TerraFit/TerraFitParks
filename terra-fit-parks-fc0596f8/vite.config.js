32243dd9b22ed5c739a7c359de407ba76e20170c
TerraFit <inquiry@aemaragroup.com>
2025-12-11 14:49:49 +0200

    Update vite.config.js

--- terra-fit-parks-fc0596f8/vite.config.js
+++ terra-fit-parks-fc0596f8/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/terra-fit-parks-fc0596f8/',
   server: {
     allowedHosts: true
   },
